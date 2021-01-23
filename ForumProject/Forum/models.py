c55b7dc744b62617999bd97b577cdc5c120d362a
Семёнов Е.В <gleb122002@gmail.com>
2021-01-23 15:01:30 +0300

    Changed DB to Postgres

--- ForumProject/Forum/models.py
+++ ForumProject/Forum/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    text = models.TextField(null=False, blank=False)
+    is_published = models.BooleanField(default=False, null=False, blank=False)
+    creator = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
