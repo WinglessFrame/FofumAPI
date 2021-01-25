a1511f20140b97c62b9107cf1a1a983c178314f3
Семёнов Е.В <gleb122002@gmail.com>
2021-01-25 19:54:13 +0300

    Fixed auth/permission classes. Added category title to post serializers

--- ForumProject/Forum/api/serializers.py
+++ ForumProject/Forum/api/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
-
+from django.contrib.auth import get_user_model
 from ..models import Post, Comment
+
+
+User = get_user_model()
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -29,7 +32,11 @@ class CommentSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     like_counter = serializers.SerializerMethodField()
     comments_count = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
     detail = serializers.SerializerMethodField()
+
+    def get_category(self, obj):
+        return obj.category.title
 
     def get_like_counter(self, obj):
         return obj.likes.count()
@@ -55,17 +62,31 @@ class PostSerializer(serializers.ModelSerializer):
             'detail',
         )
 
+
 class PostDetailSerializer(serializers.ModelSerializer):
     like_counter = serializers.SerializerMethodField()
+    like_status = serializers.SerializerMethodField()
+    like_url = serializers.SerializerMethodField()
     comments = CommentSerializer(many=True, read_only=True)
+    category = serializers.SerializerMethodField()
 
-    # def get_root_comments(self):
-    #     all_comments = [obj for obj in self.comments]
-    #     filtered = [obj for obj in all_comments if obj.is_root_node()]
-    #     return filtered
+    def get_category(self, obj):
+        return obj.category.title
 
     def get_like_counter(self, obj):
         return obj.likes.count()
+
+    def get_like_url(self, obj):
+        request = self.context.get('request')
+        return reverse('forum-api:post-like', args=[obj.pk], request=request)
+
+    def get_like_status(self, obj):
+        request = self.context.get('request')
+        user = request.user
+        if user.is_authenticated:
+            status = (obj in user.post_likes.all())
+            return status
+        else: return "Requires authentication"
 
     class Meta:
         model = Post
@@ -77,5 +98,7 @@ class PostDetailSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
             'like_counter',
+            'like_status',
+            'like_url',
             'comments',
         )
--- ForumProject/Forum/api/views.py
+++ ForumProject/Forum/api/views.py
@@ -3,33 +3,40 @@ from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework import authentication
+from django.shortcuts import redirect
 
 from ..models import Post
 from .serializers import PostSerializer, PostDetailSerializer
 
 
 class PostListAPIView(ListCreateAPIView):
-    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    #authentication_classes = []
+    authentication_classes  = [authentication.SessionAuthentication, authentication.TokenAuthentication]
+    permission_classes= [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(is_published=True)
 
 
 class PostDetailAPIView(RetrieveUpdateDestroyAPIView):
-    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    authentication_classes = []
+    authentication_classes = [authentication.SessionAuthentication, authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PostDetailSerializer
     lookup_field = 'pk'
     queryset = Post.objects.filter(is_published=True)
 
 
 class PostLikeView(APIView):
-    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
+    authentication_classes = [authentication.SessionAuthentication, authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request, pk):
         print(request.user)
-        if request.user.is_authenticated:
-            return Response(data={'You can like'}, status=200)
+        post = Post.objects.get(pk=pk)
+        user = request.user
+        if user.is_authenticated and post:
+            if post in request.user.post_likes.all():
+                return Response(data={"You have already liked this post"})
+            else:
+                user.post_likes.add(post)
+                return redirect('forum-api:post-detail', pk=pk)
         else:
             return Response(data={'You are not allowed to like'}, status=400)
